f62ddd6df3dc794df687a4afd1b3f887f7040e37
Abhishek Sharma <1859abhi@gmail.com>
2025-04-17 15:39:31 +0530

    Added Storage Account resource

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "channustorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
