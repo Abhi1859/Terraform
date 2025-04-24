fd0e538b3ee07aa51eee3b11a7f2918ea9f87855
Abhi1859 <1859abhi@gmail.com>
2025-04-24 22:51:26 +0530

    updated

--- Environement/Dev/Provider.tf
+++ Environement/Dev/Provider.tf
@@ -5,10 +5,10 @@ terraform {
       version = "4.26.0"
       # version = "~> 3.0"
     }
+    }
   }
 
-}
 provider "azurem" {
   features {}
-  subscription_id = "5c200492-f95f-421f-bd35-8e95fc857bd0"
+  subscription_id = "e2b21bd3-23b3-43c2-a854-a8c3e3fad269"
 }
